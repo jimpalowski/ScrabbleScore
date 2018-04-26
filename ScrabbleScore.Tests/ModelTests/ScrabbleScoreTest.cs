eb684a7e987ff532669d03fefe1c6bdcd8aec7be
CJ Rhone and Jim Palowski <cj@>
2018-04-26 10:36:55 -0700

    Add functionality to recognize unit in string word

--- ScrabbleScore.Tests/ModelTests/ScrabbleScoreTest.cs
+++ ScrabbleScore.Tests/ModelTests/ScrabbleScoreTest.cs
@@ -1,6 +1,9 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
 using System;
+using System.Linq;
+//using ScrabbleScore.Models;
+
 
 namespace ScrabbleScore.Tests
 {
@@ -8,9 +11,37 @@ namespace ScrabbleScore.Tests
   public class ScrabbleScoreTests
   {
     [TestMethod]
-    public void ScrabbleWord()
+    public void ScrabbleWordQ()
     {
-      
+      string _word = "QZuincy";
+      //ScrabbleCheck score = new ScrabbleCheck();
+      int score = 0;
+      foreach(char letter in _word){
+        //letter.ToArray();
+        //foreach(char letter in word){
+          //Console.WriteLine("The letters are:" + letter);
+        if(letter.Contains("Q"))
+        {
+           score++;
+
+        }
+        else
+        {
+          score += 0;
+        }
+        // else if(_word.Contains("Z"))
+        // {
+        //   score += 10;
+        // //  Console.WriteLine("Second Score:" + score);
+        // }
+        // else
+        // {
+        //   score +=1;
+        // }
+
+        Console.WriteLine("The word is:"+ letter);
+      }
+        Console.WriteLine("Score:" + score);
     }
   }
 }
---
+++ ScrabbleScore/Models/Scrabble.cs
@@ -0,0 +1,29 @@
+// using System.Collections.Generic;
+// using System;
+//
+// namespace ScrabbleScore.Models
+// {
+//   public class ScrabbleCheck
+//   {
+//     private string _word;
+//     private int _score;
+//     private static List<ScrabbleCheck> _instances = new List<ScrabbleCheck>{};
+//     public ScrabbleCheck(string word, int score)
+//     {
+//       _score = score;
+//       _word = word;
+//     }
+//     public string GetWord()
+//     {
+//       return _word;
+//     }
+//     public int GetScore()
+//     {
+//       return _score;
+//     }
+//     public static List<ScrabbleCheck> GetAll()
+//     {
+//       return _instances;
+//     }
+//   }
+// }

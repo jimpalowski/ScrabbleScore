b222c1696985a61e908530d5bf419c6525d6af9d
CJ Rhone and Jim Palowski <jim@>
2018-04-26 09:01:48 -0700

    Initialize commit with pair

---
+++ ScrabbleScore.Tests/ModelTests/ScrabbleScoreTest.cs
@@ -0,0 +1,16 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System;
+
+namespace ScrabbleScore.Tests
+{
+  [TestClass]
+  public class ScrabbleScoreTests
+  {
+    [TestMethod]
+    public void ScrabbleWord()
+    {
+      
+    }
+  }
+}
